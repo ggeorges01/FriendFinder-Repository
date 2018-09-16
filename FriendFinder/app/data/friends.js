4b01c980ff573b97023bf78e7d3f9a45e49c8f97
Gigi <g_gi84@yahoo.com>
2018-09-15 22:06:52 -0400

    modified Friend Finder App

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,25 @@
+module.exports = [{
+    "name":"Ross",
+    "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWFwIE_1TOS4gz0IIqFSQ5d5esE1m2PGU1vc2SVsQYn_eqZNSgQA",
+    "scores":[5,1,4,4,5,1,2,5,4,1]
+  }, {
+    "name":"Bill",
+    "photo":"http://cdn3-www.craveonline.com/assets/mandatory/legacy/2013/04/man_file_1043888_1-bill-murray-funniest-actors-300x300.jpg",
+    "scores":[5,1,4,4,5,1,2,5,4,1]
+  }, {
+    "name":"Hamm",
+    "photo":"https://typeset-beta.imgix.net/rehost%2F2016%2F9%2F13%2Fbc05f04a-fb52-4c6f-915a-800da9ae021c.jpg",
+    "scores":[5,1,4,4,5,1,2,5,4,1]
+  }, {
+    "name":"Fred",
+    "photo":"http://glamazonsblog.com/wp-content/uploads/2011/11/mr-rogers-red-sweater-Glamazons-Blog-2.gif",
+    "scores":[5,1,4,4,5,1,2,5,4,1]
+  }, {
+      "name":"Oprah",
+    "photo":"https://static.tvgcdn.net/mediabin/showcards/celebs/m-o/thumbs/oprah-winfrey-168611_828x1104.png",
+    "scores":[5,1,4,4,5,1,2,5,4,1]
+  }, {
+    "name":"Monica",
+    "photo":"https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-friends-courteney-cox.jpg",
+    "scores":[5,1,4,4,5,1,2,5,4,1]
+  }]
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # FriendFinder-Repository
+This app allows user to take a survey, answering 10 questions with a 1-5 scale ranging from agree to disagree. Upon submitting the survey form, the app compare the user with survey participants to identify who is the closest match. The last user who submits the form will also be added to the existing Friend Finder database.
+
+The matching is done by computing the smallest differences between each survey responder and accumulating these differences. Responders on the database that have the smallest difference will be considered to be the closest match.
+
+node.js Dependencies:
+
+express
+body-parser
+path
+
+Backend modules:
+
+server.js - sets up and starts server
+apiRoutes.js - specifies routes for API services
+htmlRoutes.js - specifies routes for HTML services
 Friend Finder Repository
